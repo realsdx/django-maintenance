974e273beb5d6a44690b6d5bfdd8d8a1e1f48903
realsdx <realsdx@protonmail.com>
2018-12-07 13:34:21 +0530

    Updated models

--- maintenance/models.py
+++ maintenance/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class MainteneceMode(models.Model):
     status = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if MainteneceMode.objects.exists() and not self.pk:
+        # if you'll not check for self.pk 
+        # then error will also raised in update of exists model
+            raise ValidationError('There is can be only one MainteneceMode instance')
+        return super(MainteneceMode, self).save(*args, **kwargs)
